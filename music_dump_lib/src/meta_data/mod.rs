6da49ef6bac70ef6aa7a6456a0ed68a8a3f51088
avaniawang <avaniawang@outlook.com>
2024-07-16 18:33:18 +0800

    guess picture type and convert to string

--- music_dump_lib/src/meta_data/mod.rs
+++ music_dump_lib/src/meta_data/mod.rs
@@ -1,3 +1,17 @@
 mod metadata_trait;
 mod ncm_metadata;
 mod qmc_metadata;
+
+use image::{guess_format, ImageFormat};
+pub use ncm_metadata::*;
+
+pub fn guess_pict_type(data: &Vec<u8>) -> String {
+    let format = guess_format(&data).unwrap_or(ImageFormat::Jpeg);
+    match format {
+        ImageFormat::Png => "png".to_owned(),
+        ImageFormat::Jpeg => "jpeg".to_owned(),
+        ImageFormat::Bmp => "bmp".to_owned(),
+        ImageFormat::Ico => "ico".to_owned(),
+        _ => "unknown".to_owned(),
+    }
+}
