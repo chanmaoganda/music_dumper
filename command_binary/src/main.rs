5edb4e15c1807c09e3ee6f27e0c2faf3af2c7be2
avaniawang <avaniawang@outlook.com>
2024-07-17 11:26:33 +0800

    fix logic and some error testing

--- command_binary/src/main.rs
+++ command_binary/src/main.rs
@@ -9,8 +9,8 @@ use music_dump_lib::NcmDumper;
 fn main() -> anyhow::Result<()> {
     let command = Command::parse();
     let music_list = command.get_items()?;
-    let output_directory = command.output_path.unwrap();
-    let ncm_dumper = NcmDumper::new(music_list, PathBuf::from(&output_directory));
+    let output_directory = PathBuf::from(command.output_path.unwrap());
+    let ncm_dumper = NcmDumper::new(music_list, output_directory);
     ncm_dumper.dump_all()?;
     Ok(())
 }
